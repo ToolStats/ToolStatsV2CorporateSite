f07fc84866f0a95458741a49b1c09d57ac9d292e
propersoftware <softwaredev@propergroupintl.com>
2017-12-11 17:00:31 -0500

    Add project files.

---
+++ ToolStats.Web.Corp/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ToolStats.Web.Corp
+{
+    public static class Configuration
+    {
+        public static string ApiServerPath => ConfigurationManager.AppSettings["ApiServerPath"];
+        public static string AppSitePath => ConfigurationManager.AppSettings["AppSitePath"];
+        public static string PaymentPortal => ConfigurationManager.AppSettings["PaymentPortal"];
+    }
+}
